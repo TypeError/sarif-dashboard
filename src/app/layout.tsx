25cdab1f27dca5e75a0f1fc2e565dd5c9a2b5683
cak <cak@typeerror.com>
2025-01-26 12:32:18 -0500

    Update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,9 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "SARIF Dashboard",
-  description: "A dashboard for viewing SARIF files",
+  title: "SARIF Dashboard: Visualize and Analyze Your Code's Health",
+  description:
+    "Dive into SARIF files with ease using our interactive dashboard. Gain actionable insights, spot vulnerabilities, and keep your code secure with advanced visualization tools.",
 };
 
 export default function RootLayout({
